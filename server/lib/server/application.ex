5a591d9957f750928016a829db9a39760e95a652
bushman77 <banolik@gmail.com>
2025-04-29 18:46:35 -0700

    Update application.ex

--- server/lib/server/application.ex
+++ server/lib/server/application.ex
@@ -1,1 +1,15 @@
+defmodule ElixirTcpServer.Application do
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      {Task.Supervisor, name: ElixirTcpServer.TaskSupervisor},
+      ElixirTcpServer.ClientRegistry,
+      ElixirTcpServer.TCP
+    ]
+
+    opts = [strategy: :one_for_one, name: ElixirTcpServer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
 
